f74f8b2107f2084a2e26150cea1b2499805b3d06
AyushKazi <ayushkazishrestha@gmail.com>
2024-01-24 22:19:26 +0545

    tailwind

--- userTable/src/App.jsx
+++ userTable/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import UserDetails from "./components/UserDetails";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <NavBar />
+      <UserDetails />
+    </>
+  );
 };
 
 export default App;
