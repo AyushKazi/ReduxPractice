c8a20617efa2ffcce00e5b579ff1b2068e3958e1
AyushKazi <ayushkazishrestha@gmail.com>
2024-01-25 13:39:20 +0545

    tailwind

--- userTable/src/App.jsx
+++ userTable/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import UserDetails from "./components/UserDetails";
+import "./index.css";
 
 const App = () => {
   return (
     <>
+      <h1 className="text-green-500">hello</h1>
       <NavBar />
       <UserDetails />
     </>
--- userTable/src/components/UserDetails.jsx
+++ userTable/src/components/UserDetails.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const UserDetails = () => {
-  return <div>UserDetails</div>;
+  return (
+    <div className=" m-10">
+      <div className="">
+        <h1>List of Users</h1>
+        <button>Add new user</button>
+      </div>
+      <ul></ul>
+    </div>
+  );
 };
 
 export default UserDetails;
